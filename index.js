ea49f780202428d963df6ffefd5c700243320876
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-20 01:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function printBadges(employeeNames) {
   }
   return employeeNames;
 }
+
+function tailsNeverFails() {
+  let count = 0;
+
+  while (Math.random() >= 0.5) {
+    count++;
+  }
+  return `You got ${count} tails in a row!`
+}
